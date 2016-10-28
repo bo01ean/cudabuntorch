ed70b5b04d0c86087e146e2048e4559369b2990c
Nathan Trujillo <kelvin.beats@gmail.com>
2016-10-28 15:43:57 -0700

    Update dockergo.sh

--- dockergo.sh
+++ dockergo.sh
@@ -1,3 +1,5 @@
 docker stop $(docker ps -a -q);
 docker rm $(docker ps -a -q);
-docker run -it -v`pwd`:/tmp  ubuntu:16.04 bash /tmp/test.sh
+docker run -it -v`pwd`:/tmp  ubuntu:16.04
+#### Run this in docker
+### /tmp/test.sh
