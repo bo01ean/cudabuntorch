c998b1e540e2a4d9fdd7b74586dfc96a008a49b5
Nathan Trujillo <NathanTrujillo@shop.local>
2017-02-15 15:45:57 -0800

    Make Docker task fire automatically.

--- dockergo.sh
+++ dockergo.sh
@@ -1,5 +1,3 @@
 docker stop $(docker ps -a -q);
 docker rm $(docker ps -a -q);
-docker run -it -v`pwd`:/tmp  ubuntu:16.04
-#### Run this in docker
-### /tmp/test.sh
+docker run -it -v`pwd`:/tmp  ubuntu:16.04 bash /tmp/test.sh
